0abad054b4439cc40c66ed454be6348d09f4590f
camou92 <cmohamed992@gmail.com>
2023-03-22 14:58:29 +0000

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { s } from "./App.style";
+import { CardTodo } from "./components/CardTodo/CardTodo";
+import { Header } from "./components/header/Header";
 
 export default function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Sortir le chien", isCompleted: true },
+    { id: 2, title: "Aller chez le garagiste", isCompleted: false },
+    { id: 3, title: "Faire les courses", isCompleted: true },
+    { id: 4, title: "Appeler le vétérinaire", isCompleted: true },
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={s.app}>
+          <View style={s.header}>
+            <Header />
+          </View>
+          <View style={s.body}>
+            <CardTodo todo={todoList[0]} />
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={s.footer}>
+        <Text>Footer</Text>
+      </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
